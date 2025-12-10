09c2bc218343e6992601d2e4716dae07c93c9c14
javakishore-veleti <javakishore@gmail.com>
2025-12-10 08:21:29 -0500

    Initial Commit

---
+++ catalog-mcp-server/src/main/java/com/jk/labs/spring_ai/pet_care/catalog/mcp/service/McpToolServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jk.labs.spring_ai.pet_care.catalog.mcp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class McpToolServiceImpl implements McpToolService{
+}
